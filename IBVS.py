9bc54bf8f4f83f0b0420bed496bf6632bc535de0
Arivoli-linux <braincraft2017@gmail.com>
2025-12-06 17:20:46 -0800

    Moving towards the puck

--- IBVS.py
+++ IBVS.py
@@ -18,11 +18,14 @@ camera_focal_depth = utils.camera_focal_depth # camera focal depth f
 
 # control objectives 
 object_location_desired = np.array([camera_width/2,camera_height/2])  #center the object to middle of image
-                                                                
+z_desired = 0
+
 K_p_x = 0.1           #Proportional control gain for translation
 K_p_Omega = 0.02      #Proportional control gain for rotation       
 
 MAX_ITER = 2000
+MAX_REACH = 0.85      # Max reach of Panda arm  
+IS_CONTACT_TOL = 1e-1
 
 
 def getImageJacobian(u_px,v_px,depthImg,focal_length, imgWidth, imgHeight):
@@ -112,7 +115,7 @@ def findJointControl(robot, delta_X, delta_Omega):
 
 
 
-def image_visual_servo(env, robot):
+def image_visual_servo(env, robot, close_depth = True):
 
     frames = []
     for ITER in range(MAX_ITER):
@@ -130,16 +133,31 @@ def image_visual_servo(env, robot):
         
         object_loc = utils.get_puck_center_from_camera(env.puck_id, segmentation_image=segment) 
 
-        # Image Jacobian
-        imageJacobian = getImageJacobian(object_loc[0], object_loc[1], depth, camera_focal_depth, camera_width, camera_height)
+        if object_loc:
+            
+            # Image Jacobian
+            imageJacobian = getImageJacobian(object_loc[0], object_loc[1], depth, camera_focal_depth, camera_width, camera_height)
 
-        # Camera control
-        delta_X, delta_Omega = findCameraControl(object_location_desired, object_loc, imageJacobian)
+            # Camera control
+            delta_X, delta_Omega = findCameraControl(object_location_desired, object_loc, imageJacobian)
+            
+            
+            delta_X = cameraOrientation @ (delta_X)
+            delta_Omega = cameraOrientation @ delta_Omega 
         
-        
-        delta_X = cameraOrientation @ delta_X 
-        delta_Omega = cameraOrientation @ delta_Omega 
-        
+            contact_err = (1.0/200) * cameraOrientation @ np.array([0, 0, z_desired - depth[object_loc[1], object_loc[0]]])
+
+            #clip contact magnitude 
+            contact_err_max = 5e-3  # meter-step limit
+            contact_err_norm = np.linalg.norm(contact_err)
+
+            if contact_err_norm > contact_err_max:
+                print(contact_err_norm)
+                contact_err = contact_err * (contact_err_max / contact_err_norm)
+
+            delta_X = delta_X - contact_err  # To close the gap between puck and EE
+
+
         #set Next Joint Targets
         new_jointPositions = findJointControl(robot, delta_X, delta_Omega)
         
@@ -150,23 +168,35 @@ def image_visual_servo(env, robot):
         q_home = np.array([0, -np.pi/4, 0, -3*np.pi/4, 0, np.pi/2, np.pi/4])
 
         q_sing = -(robot.get_current_joint_angles() - q_home) # Null space control to operate near to home configuration for jerk free movement
-        
+
         J_rob = robot.get_jacobian_at_current_position()
         J_rob_inv= utils.pseudo_inverse(J_rob)
 
-        joint_null = (np.eye(7) - J_rob_inv@J_rob)@(sing*q_sing)
+        depth_p = 0.0/15
+
+        if object_loc:
+            z_error = z_desired - depth[object_loc[1], object_loc[0]]
+            depth_err = -cameraOrientation @ np.array([0, 0, z_error])
+
+        q_depth_err = J_rob_inv[:,:3] @ depth_err
+
+        joint_null = (np.eye(7) - J_rob_inv@J_rob)@(sing*q_sing + depth_p*q_depth_err)
 
         robot.set_joint_position(new_jointPositions + joint_null)
         
-        
         # show image
-        u, v = object_loc
+        if object_loc:
+            u, v = object_loc
+
         cv2.circle(rgb, (int(camera_width/2), int(camera_height/2)), 5, (0, 255, 0), -1)
         cv2.circle(rgb, (u,v), 5, (255, 0, 255), -1)
         cv2.imshow("depth", depth)
         cv2.imshow("rgb", rgb)
         frames.append(cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR))
         cv2.waitKey(1)
+
+        if abs(z_error) < IS_CONTACT_TOL:
+            break
 
     imageio.mimsave('robot_servoing_PD.gif', frames, fps=20)
     print("Gif saved")
